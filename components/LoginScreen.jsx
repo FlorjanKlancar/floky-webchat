aebac7e2e74b472b1cc3d48436bda9dc6d072d71
FlorjanKlancar <klancar.florjan99@gmail.com>
2022-01-17 10:24:45 +0100

    Removed wc

--- components/LoginScreen.jsx
+++ components/LoginScreen.jsx
@@ -5,14 +5,14 @@ import { useMoralis } from "react-moralis";
 function LoginScreen() {
   const { authenticate } = useMoralis();
 
-  async function authWalletConnect() {
+  /*async function authWalletConnect() {
     const user = await authenticate({
       provider: "walletconnect",
       mobileLinks: ["metamask"],
       signingMessage: "Welcome!",
     });
     console.log("user", user);
-  }
+  }*/
 
   return (
     <div className="bg-black relative">
@@ -22,15 +22,6 @@ function LoginScreen() {
           onClick={authenticate}
         >
           Login to chat with metamask!
-        </button>
-      </div>
-
-      <div className="flex flex-col absolute z-50 items-center justify-center text-white h-3/6 w-full">
-        <button
-          className="w-1/2 md:w-1/3 lg:w-1/4 bg-teal-600 border-2 border-white rounded-lg p-4 font-bold animate-bounce "
-          onClick={authWalletConnect}
-        >
-          Login to chat with WalletConnect!
         </button>
       </div>
 
